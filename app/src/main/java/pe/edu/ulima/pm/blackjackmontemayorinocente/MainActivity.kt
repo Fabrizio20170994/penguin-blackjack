aef2fe79251e2ef523b40afbafda3e727508a94b
Fabrizio Montemayor <20170994@aloe.ulima.edu.pe>
2021-05-23 18:12:22 -0500

    Volteo de Cartas

--- app/src/main/java/pe/edu/ulima/pm/blackjackmontemayorinocente/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/blackjackmontemayorinocente/MainActivity.kt
@@ -2,10 +2,49 @@ package pe.edu.ulima.pm.blackjackmontemayorinocente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import pe.edu.ulima.pm.blackjackmontemayorinocente.views.CartaView
+import pe.edu.ulima.pm.blackjackmontemayorinocente.views.OnCardClickListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnCardClickListener {
+    // Mazo
+    val deck = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Obtenemos las cartas
+        val cartas = listOf<CartaView>(
+                findViewById<CartaView>(R.id.cartaJugador1),
+                findViewById<CartaView>(R.id.cartaJugador2),
+                findViewById<CartaView>(R.id.cartaJugador3),
+                findViewById<CartaView>(R.id.cartaJugador4),
+                findViewById<CartaView>(R.id.cartaMesa1),
+                findViewById<CartaView>(R.id.cartaMesa2),
+                findViewById<CartaView>(R.id.cartaMesa3),
+                findViewById<CartaView>(R.id.cartaMesa4))
+
+        // Asignamos el Listener a las Cartas
+        for (i in cartas) {
+            i.setOnCardClickListener(this)
+        }
+        // Llenamos el mazo
+        for (i in 1..4) {
+            deck.addAll((1..13))
+        }
+    }
+
+     override fun onClick(view : CartaView) {
+        // Si la carta no ha sido volteada
+        if (view.number == null) {
+            var randomIndex = (0 until deck.count()).random()
+            view.number = deck[randomIndex]
+            deck.removeAt(randomIndex)
+            // Volvemos a dibujar el View
+            view.invalidate()
+        }
     }
 }
--- app/src/main/java/pe/edu/ulima/pm/blackjackmontemayorinocente/views/CartaView.kt
+++ app/src/main/java/pe/edu/ulima/pm/blackjackmontemayorinocente/views/CartaView.kt
@@ -1,24 +1,39 @@
 package pe.edu.ulima.pm.blackjackmontemayorinocente.views
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import pe.edu.ulima.pm.blackjackmontemayorinocente.R
 
+interface OnCardClickListener {
+    fun onClick(view: CartaView)
+}
+
 class CartaView: View {
-    val paint : Paint? = null
+    var number : Int? = null
+    var paint : Paint? = null
     var width : Int? = null
     var height : Int? = null
     var centreX : Float? = null
     var centreY : Float? = null
+    private var listener : OnCardClickListener? = null
     // Creamos un Bitmap a partir del Pingüino
     val img: Bitmap? = BitmapFactory.decodeResource(resources, R.drawable.pinguino)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         // Por xml
+        paint = Paint()
+    }
+
+    fun setOnCardClickListener(listener: OnCardClickListener) {
+        this.listener = listener
+    }
+
+    // Si se toca el View, se llama a la función onClick()
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        this.listener?.onClick(this) // [ULFaceView : View] -> [OnFaceClickListener]
+        return super.onTouchEvent(event)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -31,13 +46,32 @@ class CartaView: View {
     }
 
     override fun onDraw(canvas: Canvas) {
-        // Escalamos el Pingüino
-        val  resized: Bitmap = Bitmap.createScaledBitmap(img!!, width!! / 2, width!! / 2, true)
-        // Color de Fondo
-        canvas.drawRGB(255, 153, 61)
-        // Dibujamos el Pingüino
-        canvas.drawBitmap(resized, centreX!!/2, centreY!!-width!!/4, paint)
-        invalidate()
+
+
+
+        // Si se clickeó, se dibuja el número
+        if (number != null) {
+            // Color de Fondo
+            canvas.drawRGB(255, 102, 102)
+            paint!!.color= Color.BLACK
+            paint!!.textSize = 55f
+            paint!!.style = Paint.Style.FILL
+            canvas.drawText(number.toString(), centreX!! - 20, centreY!! + 5, paint!!)
+            // Borde de Carta
+            val rectf = Rect()
+            this.getLocalVisibleRect(rectf)
+            paint!!.style = Paint.Style.STROKE
+            paint!!.strokeWidth = 5f
+            canvas.drawRect(rectf,paint!!)
+        }
+        // Sino, dibujamos el Pingüino
+        else {
+            // Color de Fondo
+            canvas.drawRGB(255, 153, 61)
+            // Escalamos el Pingüino
+            val  resized: Bitmap = Bitmap.createScaledBitmap(img!!, width!! / 2, width!! / 2, true)
+            canvas.drawBitmap(resized, centreX!! / 2, centreY!! - width!! / 4, paint)
+        }
     }
 
 }
